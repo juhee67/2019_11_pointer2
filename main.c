fc9c03bf21135053512ba1cc59c25c8883163018
juhee67 <55428174+juhee67@users.noreply.github.com>
2019-11-13 14:24:26 +0900

    initial

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(void) {
+    char buffer[8];
+    double *pd;
+    int *pi;
+    
+    pd = (double *)buffer;
+    *pd = 3.14;
+    printf("%f\n", *(double*)buffer);
+    
+    pi = (int *)buffer;
+    *pi = 123;
+    *(pi+1) = 456;
+    printf("%d %d\n", *(int*)buffer, *((int*)buffer+1));
+	return 0;
+}
