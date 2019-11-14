690339dad08184c7735b720c99ccc23737ba01ec
juhee67 <55428174+juhee67@users.noreply.github.com>
2019-11-14 16:04:48 +0900

    initial

--- main.c
+++ main.c
@@ -2,22 +2,15 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-int main(void) {
-	char *pc;
-	int *pi;
-	double *pd;
-	
-	pc = (char *)10000;
-	pi = (int *)10000;
-	pd = (double *)10000;
-	
-	printf("증가 전 : pc=%d, pi=%d, pd=%d\n", pc, pi, pd);
-	
-	pc++;
-	pi++;
-	pd++;
-	printf("증가 후 : pc=%d, pi=%d, pd=%d\n", pc, pi, pd);
+char *proverb = "All that glisters is not gold.";
+void setPointer(char **q)	
+{
+	*q = proverb;
+}	
+int main(void) {	
+	char *p = "zzz";
+	setPointer(&p);
+	printf("%s\n", p);
 	
 	return 0;
 }
